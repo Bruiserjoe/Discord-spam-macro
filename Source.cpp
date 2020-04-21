156a5aa6c91e7e853d45251c320f26d44ee24ebb
Bruiserjoe <42724327+Bruiserjoe@users.noreply.github.com>
2020-04-21 17:01:18 -0500

    0.82
    cleaned up a bit

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,28 @@
 #include "header.h"
-
-
 char input[1000];
 bool exitf = false;
+void setForeground() {
+	LPCSTR windowname = "Discord (32 bit)";
+	HWND dWND = FindWindow(NULL, windowname);
+	
 
+}
+//Sets the mouse position
+void setMouse() {
+	LPCSTR windowname = "Discord (32 bit)";
+	HWND dWND = FindWindow(NULL, windowname);
+	SetCursorPos(998, 859);
+	INPUT input1 = { 0 };
+	input1.type = INPUT_MOUSE;
+	input1.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	SendInput(1, &input1, sizeof(input1));
+	ZeroMemory(&input1, sizeof(input1));
+	input1.type = INPUT_MOUSE;
+	input1.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+
+}
+//Presses enter
 void enter() {
 	INPUT ino;
 	ino.type = INPUT_KEYBOARD;
@@ -14,8 +33,6 @@ void enter() {
 	SendInput(1, &ino, sizeof(ino));
 
 }
-
-
 //Types out message and sends it
 void spam(char in[1000]) {
 	for (int i = 0; i < strlen(in); i++) {
@@ -30,30 +47,18 @@ void spam(char in[1000]) {
 	enter();
 
 }
-
 int main() {
 	std::cout << "Input text you want to spam: ";
 	std::cin >> input;
 	std::cout << "Starting spam" << std::endl;
 	while (!exitf) {
-			LPCSTR windowname = "Discord (32 bit)";
-			HWND dWND = FindWindow(NULL, windowname);
-			//Sets mouse position and clicks on text box
-			SetCursorPos(998, 859);
-			INPUT input1 = { 0 };
-			input1.type = INPUT_MOUSE;
-			input1.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-			SendInput(1, &input1, sizeof(input1));
-			ZeroMemory(&input1, sizeof(input1));
-			input1.type = INPUT_MOUSE;
-			input1.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-			while (!exitf) {
-				if (GetAsyncKeyState(VK_DOWN)) {
-					exitf = true;
-				}
-				Sleep(500);
-				spam(input);
-			}
+		setMouse();
+		if (GetAsyncKeyState(VK_DOWN)) {
+				exitf = true;
+		}
+		Sleep(500);
+		spam(input);
+			
 	}
 	return 0;
 }
